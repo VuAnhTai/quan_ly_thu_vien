0ed29e3e1d993607c20e6ca6e41e5074103019c5
VAT <vuanhtai1997@gmail.com>
2018-05-19 06:45:27 +0700

    handlebars

---
+++ app.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var exphbs = require('express-handlebars');
+var exphbs_section = require('express-handlebars-sections');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+var app = express();
+
+app.engine('hbs', exphbs({
+    defaultLayout: 'main',
+    layoutsDir: 'views/_layouts/',
+    helpers: {
+        section: exphbs_section()
+    }
+}));
+app.set('view engine', 'hbs');
+
+app.use(express.static(
+    path.resolve(__dirname, 'public')
+));
+
+app.get('/', (req,res)=>{
+    res.render('users/users_view');
+})
+app.listen(3000, () => {
+    console.log('server running on port 3000');
+});
